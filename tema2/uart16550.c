0bfc9f496b363606b578e10ac3d81ec2bdbc1f80
Bogdan Tudorache <bogdanmihait10@gmail.com>
2025-01-24 14:53:26 +0200

    add tema2  77/92 impl

--- tema2/uart16550.c
+++ tema2/uart16550.c
@@ -1,10 +1,3 @@
-// SPDX-License-Identifier: GPL-2.0+
-
-/*
- * uart16550.c - Driver implementation for the 16550 UART.
- *
- * Author: Bogdan-Mihai Tudorache <bogdanmihait10@gmail.com>
- */
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/kernel.h>
@@ -16,306 +9,361 @@
 #include <linux/ioport.h>
 #include <linux/interrupt.h>
 #include <linux/spinlock.h>
-#include <linux/string.h>
 #include <linux/moduleparam.h>
 
 #include "uart16550.h"
 
-#define MODULE_NAME		"uart16550"
-#define OPTION_BOTH_STRING	"OPTION_BOTH"
-#define OPTION_COM1_STRING	"OPTION_COM1"
-#define OPTION_COM2_STRING	"OPTION_COM2"
+#define MODULE_NAME     "uart16550"
+#define COM1_ADDR      0x3f8
+#define COM2_ADDR      0x2f8
+#define PORT_SIZE      8
 
-#define COM1_ADDR 0x3f8
-#define COM2_ADDR 0x2f8
-#define NUM_PORTS 8
+#define IRQ_COM1       4
+#define IRQ_COM2       3
 
-#define COM1_PORT_NAME "COM1"
-#define COM2_PORT_NAME "COM2"
+/* Register offsets */
+#define REG_RBR     0   /* Receive Buffer Register (read) */
+#define REG_THR     0   /* Transmitter Holding Register (write) */
+#define REG_DLL     0   /* Divisor Latch LSB */
+#define REG_DLM     1   /* Divisor Latch MSB */
+#define REG_IER     1   /* Interrupt Enable Register */
+#define REG_IIR     2   /* Interrupt Identification Register */
+#define REG_FCR     2   /* FIFO Control Register */
+#define REG_LCR     3   /* Line Control Register */
+#define REG_MCR     4   /* Modem Control Register */
+#define REG_LSR     5   /* Line Status Register */
+#define REG_MSR     6   /* Modem Status Register */
 
-#define IRQ_COM1 4
-#define IRQ_COM2 3
+/* Status register bits */
+#define LSR_DR      0x01    /* Data Ready */
+#define LSR_THRE    0x20    /* Transmitter Holding Register Empty */
 
-#define COM1_DEVICE_NUM 0
-#define COM2_DEVICE_NUM 1
+#define FIFO_SIZE   512
 
-#define FIFO_SIZE	512
-#define MINOR_NUM	0
-
-static int MAJOR = 42;
-static char* OPTION = OPTION_BOTH_STRING;
+/* Module parameters */
+static int major = 42;
 static int option = OPTION_BOTH;
 
-module_param(MAJOR, int, 0000);
-MODULE_PARM_DESC(MAJOR, "Device major version");
+module_param(major, int, 0644);
+MODULE_PARM_DESC(major, "Major number (default=42)");
+module_param(option, int, 0644);
+MODULE_PARM_DESC(option, "Port option: 1=COM1, 2=COM2, 3=both (default=3)");
 
-module_param(OPTION, charp, 0000);
-MODULE_PARM_DESC(OPTION, "Options for using the device ports");
-
-
-struct uart16550_data {
-	struct cdev cdev;
-	int device_num;
-	spinlock_t lock;
-	// TODO: see if atomic_t types are needed (kfifo type should already have synchonized methods)
-	atomic_t read_buffer_size, write_buffer_size;
-    wait_queue_head_t wq_reads, wq_writes;
-	DECLARE_KFIFO(read_fifo, unsigned char, FIFO_SIZE)
-	DECLARE_KFIFO(write_fifo, unsigned char, FIFO_SIZE)
-} devs[MAX_NUMBER_DEVICES];
-
-
-/*
- * Return the value of the DATA register.
- */
-static inline u8 i8042_read_data(void)
-{
-	// u8 val;
-	// val = inb(I8042_DATA_REG);
-	// return val;
-	return 0;
-}
-
-
-irqreturn_t uart16550_interrupt_handle(int irq_no, void *dev_id)
-{
-	// TODO: A lot to do here;
-	// - see which port is accesed
-	// - detect and handle read or write interrupts
-	// - notify finished intrerupt
-
-	// unsigned int scancode = 0;
-	// int pressed, ch;
-
-	// scancode = i8042_read_data();
-	// pressed = is_key_press(scancode);
-	// ch = get_ascii(scancode);
-
-	// pr_info("IRQ %d: scancode=0x%x (%u) pressed=%d ch=%c\n",
-	// 	irq_no, scancode, scancode, pressed, ch);
-
-	// if (pressed) {
-	// 	struct uart16550 *data = (struct uart16550 *)dev_id;
-
-	// 	spin_lock(&data->lock);
-	// 	put_char(data, ch);
-	// 	spin_unlock(&data->lock);
-	// }
-
-	return IRQ_NONE;
-}
-
-static int uart16550_open(struct inode *inode, struct file *file)
-{
-	struct uart16550 *data = container_of(inode->i_cdev, struct uart16550, cdev);
-	file->private_data = data;
-	pr_info("%s opened\n", MODULE_NAME);
-	return 0;
-}
-
-static int uart16550_release(struct inode *inode, struct file *file)
-{
-	pr_info("%s closed\n", MODULE_NAME);
-	return 0;
-}
-
-/* TODO 5/12: add write operation and reset the buffer */
-static ssize_t uart16550_write(struct file *file, const char __user *user_buffer,
-			 size_t size, loff_t *offset)
-{
-	struct uart16550 *data = (struct uart16550 *) file->private_data;
-	unsigned long flags;
-
-	spin_lock_irqsave(&data->lock, flags);
-	// reset_buffer(data);
-	spin_unlock_irqrestore(&data->lock, flags);
-
-	return size;
-}
-
-static ssize_t uart16550_read(struct file *file,  char __user *user_buffer,
-			size_t size, loff_t *offset)
-{
-	struct uart16550 *data = (struct uart16550 *) file->private_data;
-	size_t read = 0;
-	/* TODO 4/18: read data from buffer */
-	unsigned long flags;
-	char ch;
-	bool more = true;
-
-	while (size--) {
-		spin_lock_irqsave(&data->lock, flags);
-		// more = get_char(&ch, data);
-		spin_unlock_irqrestore(&data->lock, flags);
-		read++;
-	}
-
-	return read;
-}
-
-static long uart16550_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
-{
-	int ret = 0;
-
-	switch (cmd) {
-	case UART16550_IOCTL_SET_LINE:
-		// TODO: complete this
-		break;
-	default:
-		ret = -EINVAL;
-	}
-
-	return ret;
-}
-
-static const struct file_operations uart16550_fops = {
-	.owner = THIS_MODULE,
-	.open = uart16550_open,
-	.release = uart16550_release,
-	.read = uart16550_read,
-	.write = uart16550_write,
-	.unlocked_ioctl = uart16550_ioctl
+struct uart_port {
+    unsigned int            iobase;
+    int                    irq;
+    const char            *name;
+    spinlock_t             lock;
+    DECLARE_KFIFO(rxfifo, unsigned char, FIFO_SIZE);
+    DECLARE_KFIFO(txfifo, unsigned char, FIFO_SIZE);
+    wait_queue_head_t      rxwait;
+    wait_queue_head_t      txwait;
+    struct cdev            cdev;
 };
 
-static int uart16550_init(void)
+static struct uart_port ports[2];
+
+/* Hardware access functions */
+static inline unsigned char io_read(struct uart_port *port, int reg)
 {
-	int err;
-	int num_minors = MAX_NUMBER_DEVICES;
-
-	if (strncmp(OPTION, OPTION_BOTH_STRING, strlen(OPTION_BOTH_STRING)) == 0) {
-		option = OPTION_BOTH;
-		num_minors = MAX_NUMBER_DEVICES
-	} else if (strncmp(OPTION, OPTION_COM1_STRING, strlen(OPTION_COM1_STRING)) == 0) {
-		option = OPTION_COM1;
-		num_minors = 1;
-	} else if (strncmp(OPTION, OPTION_COM2_STRING, strlen(OPTION_COM2_STRING)) == 0) {
-		option = OPTION_COM2
-		num_minors = 1;
-	}
-
-	err = register_chrdev_region(MKDEV(MAJOR, MINOR_NUM), num_minors, MODULE_NAME);
-	if (err != 0) {
-		pr_err("register_region failed: %d\n", err);
-		goto out;
-	}
-
-	if (option == OPTION_BOTH) {
-		if (request_region(COM1_ADDR, NUM_PORTS, COM1_PORT_NAME) == NULL) {
-			err = -EBUSY;
-			goto out_unregister;
-		}
-
-		if (request_region(COM2_ADDR, NUM_PORTS, COM2_PORT_NAME) == NULL) {
-			err = -EBUSY;
-			goto out_release_region;
-		}
-	} else if (option == OPTION_COM1 && request_region(COM1_ADDR, NUM_PORTS, COM1_PORT_NAME) == NULL) {
-		err = -EBUSY;
-		goto out_unregister;
-  	} else if (option == OPTION_COM2 && request_region(COM2_ADDR, NUM_PORTS, COM2_PORT_NAME) == NULL) {
-		err = -EBUSY;
-		goto out_release_region;
-	}
-
-	
-	if (option == OPTION_BOTH) {
-		err = request_irq(IRQ_COM1, uart16550_interrupt_handle, IRQF_SHARED, COM1_PORT_NAME, &devs[0]);
-		if (err != 0) {
-			pr_err("request_irq failed: %d\n", err);
-			goto out_release_regions;
-		}
-
-		err = request_irq(IRQ_COM2, uart16550_interrupt_handle, IRQF_SHARED, COM2_PORT_NAME, &devs[1]);
-		if (err != 0) {
-			pr_err("request_irq failed: %d\n", err);
-			goto out_release_irq_com1;
-		}
-	} else if (option == OPTION_COM1) {
-		err = request_irq(IRQ_COM1, uart16550_interrupt_handle, IRQF_SHARED, COM1_PORT_NAME, &devs[0]);
-		if (err != 0) {
-			pr_err("request_irq failed: %d\n", err);
-			goto out_release_regions;
-		}
-	} else if (option == OPTION_COM2) {
-		err = request_irq(IRQ_COM2, uart16550_interrupt_handle, IRQF_SHARED, COM2_PORT_NAME, &devs[1]);
-		if (err != 0) {
-			pr_err("request_irq failed: %d\n", err);
-			goto out_release_irq_com1;
-		}
-	}
-
-	if (option == OPTION_BOTH) {
-		devs[0].device_num = COM1_DEVICE_NUM;
-		devs[1].device_num = COM2_DEVICE_NUM;
-	} else if (option == OPTION_COM1) {
-		devs[0].device_num = COM1_DEVICE_NUM;
-	} else if (option == OPTION_COM2) {
-		devs[1].device_num = COM2_DEVICE_NUM;
-	}
-
-	for (int i = 0; i < num_minors; i++) {
-		spin_lock_init(&devs[i].lock);
-		init_waitqueue_head(&devs[i].wq_reads);
-		init_waitqueue_head(&devs[i].wq_writes);
-		INIT_KFIFO(devs[i].read_fifo);
-		INIT_KFIFO(devs[i].write_fifo);
-		cdev_init(&devs[i].cdev, &uart16550_fops);
-		cdev_add(&devs[i].cdev, MKDEV(MAJOR, i), 1);
-	}
-
-	return 0;
-
-out_release_irq_com1
-	free_irq(I8042_KBD_IRQ, &devs[0]);
-
-out_release_regions:
-	release_region(I8042_STATUS_REG+1, 1);
-out_release_region:
-	release_region(I8042_DATA_REG+1, 1);
-
-out_unregister:
-	unregister_chrdev_region(MKDEV(MAJOR, MINOR_NUM), num_minors);
-out:
-	return err;
+    return inb(port->iobase + reg);
 }
 
-static void uart16550_exit(void)
+static inline void io_write(struct uart_port *port, int reg, unsigned char value)
 {
-	if (option == OPTION_BOTH) {
-		cdev_del(&devs[0].cdev);
-		cdev_del(&devs[1].cdev);
+    outb(value, port->iobase + reg);
+}
 
-		free_irq(IRQ_COM1, &devs[0]);
-		free_irq(IRQ_COM2, &devs[1]);
+/* Initialize UART hardware */
+static void hw_init(struct uart_port *port)
+{
+    unsigned long flags;
 
-		release_region(COM1_ADDR, NUM_PORTS);
-		release_region(COM2_ADDR, NUM_PORTS);
+    spin_lock_irqsave(&port->lock, flags);
 
-		unregister_chrdev_region(MKDEV(MAJOR, MINOR_NUM), 2);
-	} else if (option == OPTION_COM1) {
-		cdev_del(&devs[0].cdev);
+    /* Disable all interrupts */
+    io_write(port, REG_IER, 0);
 
-		free_irq(IRQ_COM1, &devs[0]);
+    /* Enable and clear FIFOs */
+    io_write(port, REG_FCR, 0x07);
 
-		release_region(COM1_ADDR, NUM_PORTS);
+    /* 8N1 mode */
+    io_write(port, REG_LCR, 0x03);
 
-		unregister_chrdev_region(MKDEV(MAJOR, MINOR_NUM), 1);
-	} else if (option == OPTION_COM2) {
-		cdev_del(&devs[1].cdev);
+    /* Set baud rate to 9600 */
+    io_write(port, REG_LCR, io_read(port, REG_LCR) | 0x80);  /* Set DLAB */
+    io_write(port, REG_DLL, 12);  /* 115200/9600 = 12 */
+    io_write(port, REG_DLM, 0);
+    io_write(port, REG_LCR, io_read(port, REG_LCR) & ~0x80); /* Clear DLAB */
 
-		free_irq(IRQ_COM2, &devs[1]);
+    /* Enable RX and TX interrupts */
+    io_write(port, REG_IER, 0x03);
 
-		release_region(COM2_ADDR, NUM_PORTS);
+    spin_unlock_irqrestore(&port->lock, flags);
+}
 
-		unregister_chrdev_region(MKDEV(MAJOR, MINOR_NUM), 1);
-	}
+static irqreturn_t uart_interrupt(int irq, void *dev_id)
+{
+    struct uart_port *port = dev_id;
+    unsigned char iir, lsr;
+    char ch;
+    unsigned long flags;
 
-	pr_notice("Driver %s unloaded\n", MODULE_NAME);
+    spin_lock_irqsave(&port->lock, flags);
+
+    iir = io_read(port, REG_IIR) & 0x0f;
+
+    switch (iir) {
+    case 0x04: /* Received data */
+        while ((lsr = io_read(port, REG_LSR)) & LSR_DR) {
+            ch = io_read(port, REG_RBR);
+            kfifo_in(&port->rxfifo, &ch, 1);
+        }
+        wake_up_interruptible(&port->rxwait);
+        break;
+
+    case 0x02: /* Transmitter empty */
+        if (kfifo_out(&port->txfifo, &ch, 1))
+            io_write(port, REG_THR, ch);
+        wake_up_interruptible(&port->txwait);
+        break;
+    }
+
+    spin_unlock_irqrestore(&port->lock, flags);
+    return IRQ_HANDLED;
+}
+
+static int uart_open(struct inode *inode, struct file *file)
+{
+    struct uart_port *port = container_of(inode->i_cdev, struct uart_port, cdev);
+    file->private_data = port;
+    return 0;
+}
+
+static int uart_release(struct inode *inode, struct file *file)
+{
+    return 0;
+}
+
+static ssize_t uart_read(struct file *file, char __user *buf,
+                       size_t count, loff_t *ppos)
+{
+    struct uart_port *port = file->private_data;
+    unsigned int copied;
+
+    if (wait_event_interruptible(port->rxwait,
+                kfifo_len(&port->rxfifo) > 0))
+        return -ERESTARTSYS;
+
+    if (kfifo_to_user(&port->rxfifo, buf, count, &copied))
+        return -EFAULT;
+
+    return copied;
+}
+
+static ssize_t uart_write(struct file *file, const char __user *buf,
+                        size_t count, loff_t *ppos)
+{
+    struct uart_port *port = file->private_data;
+    unsigned int copied;
+    char ch;
+
+    if (kfifo_from_user(&port->txfifo, buf, count, &copied))
+        return -EFAULT;
+
+    /* Start transmission if TX empty */
+    if (io_read(port, REG_LSR) & LSR_THRE) {
+        if (kfifo_out(&port->txfifo, &ch, 1))
+            io_write(port, REG_THR, ch);
+    }
+
+    return copied;
+}
+
+static long uart_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
+{
+    struct uart_port *port = file->private_data;
+    struct uart16550_line_info line;
+    unsigned long flags;
+
+    if (cmd != UART16550_IOCTL_SET_LINE)
+        return -ENOTTY;
+
+    if (copy_from_user(&line, (void __user *)arg, sizeof(line)))
+        return -EFAULT;
+
+    spin_lock_irqsave(&port->lock, flags);
+
+    /* Set DLAB and configure baud rate */
+    io_write(port, REG_LCR, io_read(port, REG_LCR) | 0x80);
+    io_write(port, REG_DLL, line.baud & 0xff);
+    io_write(port, REG_DLM, (line.baud >> 8) & 0xff);
+
+    /* Configure line parameters and clear DLAB */
+    io_write(port, REG_LCR, (line.len & 0x03) |
+                          ((line.stop & 0x01) << 2) |
+                          (line.par & 0x38));
+
+    spin_unlock_irqrestore(&port->lock, flags);
+    return 0;
+}
+
+static const struct file_operations uart_fops = {
+    .owner          = THIS_MODULE,
+    .open           = uart_open,
+    .release        = uart_release,
+    .read           = uart_read,
+    .write          = uart_write,
+    .unlocked_ioctl = uart_ioctl,
+};
+
+static int init_port(struct uart_port *port, int minor)
+{
+    int ret;
+
+    spin_lock_init(&port->lock);
+    init_waitqueue_head(&port->rxwait);
+    init_waitqueue_head(&port->txwait);
+    INIT_KFIFO(port->rxfifo);
+    INIT_KFIFO(port->txfifo);
+
+    /* Request I/O region before doing anything else */
+    if (!request_region(port->iobase, PORT_SIZE, port->name)) {
+        pr_err("uart16550: cannot get IO region %x\n", port->iobase);
+        ret = -EBUSY;
+        goto fail_io;
+    }
+
+    /* Request IRQ next */
+    ret = request_irq(port->irq, uart_interrupt, IRQF_SHARED,
+                     port->name, port);
+    if (ret) {
+        pr_err("uart16550: cannot get IRQ %d\n", port->irq);
+        goto fail_irq;
+    }
+
+    /* Register character device */
+    cdev_init(&port->cdev, &uart_fops);
+    port->cdev.owner = THIS_MODULE;
+    ret = cdev_add(&port->cdev, MKDEV(major, minor), 1);
+    if (ret) {
+        pr_err("uart16550: cannot add character device\n");
+        goto fail_cdev;
+    }
+
+    /* Initialize hardware last */
+    hw_init(port);
+
+    return 0;
+
+fail_cdev:
+    free_irq(port->irq, port);
+fail_irq:
+    release_region(port->iobase, PORT_SIZE);
+fail_io:
+    return ret;
+}
+
+static void cleanup_port(struct uart_port *port)
+{
+    cdev_del(&port->cdev);
+    free_irq(port->irq, port);
+    release_region(port->iobase, PORT_SIZE);
+}
+
+static int __init uart16550_init(void)
+{
+    int ret;
+    int num_devices = 0;  /* Number of devices to register */
+    int first_minor = 0;  /* First minor number */
+
+    /* Validate option parameter */
+    if (option != OPTION_COM1 && option != OPTION_COM2 && option != OPTION_BOTH) {
+        pr_err("uart16550: invalid option parameter (must be 1, 2, or 3)\n");
+        return -EINVAL;
+    }
+
+    /* Determine the number of devices and first minor number based on the option */
+    if (option == OPTION_BOTH) {
+        num_devices = 2;  /* Register both COM1 and COM2 */
+        first_minor = 0;  /* Minors 0 and 1 */
+    } else if (option == OPTION_COM1) {
+        num_devices = 1;  /* Register only COM1 */
+        first_minor = 0;  /* Minor 0 */
+    } else if (option == OPTION_COM2) {
+        num_devices = 1;  /* Register only COM2 */
+        first_minor = 1;  /* Minor 1 */
+    }
+
+    /* Register character device region */
+    ret = register_chrdev_region(MKDEV(major, first_minor), num_devices, MODULE_NAME);
+    if (ret < 0) {
+        pr_err("uart16550: cannot register char device region\n");
+        return ret;
+    }
+
+    /* Initialize requested ports */
+    if (option == OPTION_COM1 || option == OPTION_BOTH) {
+        ports[0].iobase = COM1_ADDR;
+        ports[0].irq = IRQ_COM1;
+        ports[0].name = "uart16550_com1";
+        ret = init_port(&ports[0], first_minor);
+        if (ret)
+            goto fail_init;
+    }
+
+    if (option == OPTION_COM2 || option == OPTION_BOTH) {
+        ports[1].iobase = COM2_ADDR;
+        ports[1].irq = IRQ_COM2;
+        ports[1].name = "uart16550_com2";
+        ret = init_port(&ports[1], (option == OPTION_BOTH) ? (first_minor + 1) : first_minor);
+        if (ret) {
+            if (option == OPTION_BOTH)
+                cleanup_port(&ports[0]);
+            goto fail_init;
+        }
+    }
+
+    pr_info("uart16550: initialized with major=%d, option=%d\n",
+            major, option);
+    return 0;
+
+fail_init:
+    unregister_chrdev_region(MKDEV(major, first_minor), num_devices);
+    return ret;
+}
+
+static void __exit uart16550_exit(void)
+{
+    int num_devices = 0;  /* Number of devices to unregister */
+    int first_minor = 0;  /* First minor number */
+
+    /* Determine the number of devices and first minor number based on the option */
+    if (option == OPTION_BOTH) {
+        num_devices = 2;  /* Unregister both COM1 and COM2 */
+        first_minor = 0;  /* Minors 0 and 1 */
+    } else if (option == OPTION_COM1) {
+        num_devices = 1;  /* Unregister only COM1 */
+        first_minor = 0;  /* Minor 0 */
+    } else if (option == OPTION_COM2) {
+        num_devices = 1;  /* Unregister only COM2 */
+        first_minor = 1;  /* Minor 1 */
+    }
+
+    /* Clean up requested ports */
+    if (option == OPTION_COM2 || option == OPTION_BOTH)
+        cleanup_port(&ports[1]);
+    if (option == OPTION_COM1 || option == OPTION_BOTH)
+        cleanup_port(&ports[0]);
+
+    /* Unregister character device region */
+    unregister_chrdev_region(MKDEV(major, first_minor), num_devices);
+    pr_info("uart16550: cleaned up\n");
 }
 
 module_init(uart16550_init);
 module_exit(uart16550_exit);
 
-MODULE_DESCRIPTION("UART 16550A driver");
-MODULE_AUTHOR("Bogdan-Mihai Tudorache");
-MODULE_LICENSE("GPL v2");
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Tudorache Bogdan");
+MODULE_DESCRIPTION("16550 UART Driver");
